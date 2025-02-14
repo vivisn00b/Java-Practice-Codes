74b543970faf623ac83b106dc625d0f814f3ab63
Viv <121664534+vivisn00b@users.noreply.github.com>
2025-02-15 03:39:41 +0530

    Update arrfunctions.java

--- Arrays/arrfunctions.java
+++ Arrays/arrfunctions.java
@@ -4,6 +4,10 @@ import java.util.Arrays;
 
 public class arrfunctions {
     public static void main(String[] args) {
+        // Declaring array literal  
+        //int[] intArray = new int[]{ 1,2,3,4,5,6,7,8,9,10 }; 
+        //System.out.println(Arrays.toString(new int[]{1,2,3,4,5}));
+        
         int[] numbers = {3, 5, 1, 4, 2};
 
         // Arrays.toString() - Prints the array as a string
