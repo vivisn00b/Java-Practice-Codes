af869b6f3a1a8756106c9d707acb4c43b06f61f5
vivisn00b <vivek.d1574@gmail.com>
2025-03-02 21:10:41 +0530

    all types of classes

--- OOPs/src/com/viv/InnerClass/anonymousInnerClass.java
+++ OOPs/src/com/viv/InnerClass/anonymousInnerClass.java
@@ -1,6 +1,37 @@
+// Anonymous inner classes are declared without any name at all
+// They can be created in two ways:
+// 1. As a subclass of the specified type
+// 2. As an implementer of the specified interface
+
+//Example of 1
 package OOPs.src.com.viv.InnerClass;
 
 import java.util.*;
 
-public class anonymousInnerClass {
+class anon {
+    void show()
+    {
+        System.out.println("i am in show method of superclass");
+    }
+}
+
+// Main class
+class anonymousInnerClass {
+
+    //  An anonymous class with "anon" as base class
+    static anon a = new anon() {
+        // Anonymous inner class overriding show method
+        void show() {
+            // Calling method show() via super keyword
+            // which refers to parent class
+            super.show();
+            System.out.println("i am in a subclass's show method");
+        }
+    };
+
+    public static void main(String[] args)
+    {
+        // Calling show() method inside main() method
+        a.show();
+    }
 }
