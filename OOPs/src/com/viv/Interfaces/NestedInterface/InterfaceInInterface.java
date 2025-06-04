1dd629fea2a87c965a78370bb4ecf9a0a9970703
Viv <121664534+vivisn00b@users.noreply.github.com>
2025-06-05 01:08:44 +0530

    Interface
    nested interface

---
+++ OOPs/src/com/viv/Interfaces/NestedInterface/InterfaceInInterface.java
@@ -0,0 +1,20 @@
+interface Outer {
+    void display();
+
+    interface Nested {
+        void message();
+    }
+}
+
+class Demo implements Outer.Nested {
+    public void message() {
+        System.out.println("Nested Interface inside Interface");
+    }
+}
+
+public class InterfaceInInterface {
+    public static void main(String[] args) {
+        Outer.Nested obj = new Demo();
+        obj.message();  // Output: Nested Interface inside Interface
+    }
+}
