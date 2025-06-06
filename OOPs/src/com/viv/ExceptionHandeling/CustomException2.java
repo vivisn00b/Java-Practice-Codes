0d5362f877220850ce524ed791be1f8a1c80b28a
Viv <121664534+vivisn00b@users.noreply.github.com>
2025-06-07 02:22:08 +0530

    custom exceptions

---
+++ OOPs/src/com/viv/ExceptionHandeling/CustomException2.java
@@ -0,0 +1,34 @@
+// Extend the RuntimeException class (for unchecked exceptions).
+
+package OOPs.src.com.viv.ExceptionHandeling;
+
+class NegativeAmountException extends RuntimeException {
+    public NegativeAmountException(String msg) {
+        super(msg);
+    }
+}
+
+//class Bank {
+//    public static void deposit(int amount) {
+//        if (amount < 0)
+//            throw new NegativeAmountException("Amount cannot be negative!");
+//        else
+//            System.out.println("Deposited: ₹"+amount);
+//    }
+//}
+
+class Bank { //Using throws keyword
+    public static void deposit(int amount) throws NegativeAmountException {
+        if (amount < 0)
+            throw new NegativeAmountException("Amount cannot be negative!");
+        else
+            System.out.println("Deposited: ₹"+amount);
+    }
+}
+
+public class CustomException2 {
+    public static void main(String[] args) {
+        Bank.deposit(100);
+        Bank.deposit(-100);
+    }
+}
