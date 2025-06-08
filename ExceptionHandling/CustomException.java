61f2523808c798b039805608cbb1f6142e82cc8b
Viv <121664534+vivisn00b@users.noreply.github.com>
2025-06-08 20:16:14 +0530

    Exception handling in java

---
+++ ExceptionHandling/CustomException.java
@@ -0,0 +1,29 @@
+// User-Defined Custom Exception or custom exception is creating our own exception class
+// and throwing that exception using the "throw" keyword.
+
+// Extend the Exception class (for checked exceptions)
+
+package ExceptionHandling;
+
+class AgeTooSmallException extends Exception {
+    public AgeTooSmallException(String message) {
+        super(message);
+    }
+}
+
+public class CustomException {
+    public static void checkAge(int age) throws AgeTooSmallException {
+        if (age<18) {
+            throw new AgeTooSmallException("Age must be 18 or above");
+        } else
+            System.out.println("You're eligible!");
+    }
+
+    public static void main(String[] args) {
+        try {
+            checkAge(15);
+        } catch (AgeTooSmallException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
