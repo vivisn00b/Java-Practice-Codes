45706343040a8c1b09716f6b1cf1e2da04a8ff49
Viv <121664534+vivisn00b@users.noreply.github.com>
2025-06-19 21:49:53 +0530

    enums(enumerations) in java

--- OOPs/src/com/viv/Enums/DayTest2.java
+++ OOPs/src/com/viv/Enums/DayTest2.java
@@ -4,7 +4,22 @@ public class DayTest2 {
     public static void main(String[] args) {
         Day2 tues = Day2.TUESDAY;
         Day2 wed = Day2.WEDNESDAY;
+        Day2 day = Day2.SUNDAY;
         tues.display();
         System.out.println(wed.lower);
+//        switch (day){
+//            case SUNDAY -> System.out.println("HOLIDAY!!");
+//            case MONDAY -> System.out.println("Work Day :(");
+//            default -> throw new IllegalStateException("Unexpected value: " + day);
+//        }
+        String res = switch (day) {
+            case SUNDAY -> "S";
+            case MONDAY -> "M";
+            case TUESDAY -> "T";
+            case WEDNESDAY -> "W";
+            case THURSDAY -> null;
+            case FRIDAY -> null;
+            case SATURDAY -> null;
+        };
     }
 }
