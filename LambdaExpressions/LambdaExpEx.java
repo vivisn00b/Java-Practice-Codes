b67ac8891b041ccacd152ac75536fa4d7f1b1647
Viv <121664534+vivisn00b@users.noreply.github.com>
2025-08-07 00:12:40 +0530

    Multi Threading (Deadlock)

--- LambdaExpressions/LambdaExpEx.java
+++ LambdaExpressions/LambdaExpEx.java
@@ -1,9 +1,12 @@
 // It represents the instances of functional interfaces (interfaces with a single abstract method).
+// https://devcookies.medium.com/a-complete-guide-to-lambda-expressions-in-java-0aea2e1cea42
 
 package LambdaExpressions;
 
 interface FunInterface {
-    void asbstractFun(int x);
+    //void abstractFun(int x);
+    //void abstractFun(int x, int y);
+    void abstractFun(String st);
 
     default void normalFun() {
         System.out.println("Inside functional interface");
@@ -12,7 +15,15 @@ interface FunInterface {
 
 public class LambdaExpEx {
     public static void main(String[] args) {
-        FunInterface obj = (int x) -> System.out.println(2*x);
-        obj.asbstractFun(5);
+//        FunInterface obj = (int x) -> System.out.println(2*x);
+//        obj.abstractFun(5);
+//        FunInterface obj = (int x, int y) -> System.out.println(x*y);
+//        obj.abstractFun(5, 6);
+
+        FunInterface obj = st -> {
+            String message = "Hello, " + st + "!";
+            System.out.println(message);
+        };
+        obj.abstractFun("Viv");
     }
 }
