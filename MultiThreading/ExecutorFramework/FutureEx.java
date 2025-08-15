a4e2f49d35dc15f8761c6f6b808f786389b04f69
vivisn00b <vivek.d1574@gmail.com>
2025-08-15 21:47:31 +0530

    Multithreading (Executor framework)

--- MultiThreading/ExecutorFramework/FutureEx.java
+++ MultiThreading/ExecutorFramework/FutureEx.java
@@ -1,15 +1,19 @@
 package MultiThreading.ExecutorFramework;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class FutureEx {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService exe = Executors.newSingleThreadExecutor();
-        Future<Integer> future = exe.submit(() -> 42);
+        //Future<Integer> future = exe.submit(() -> 42);
+        //Future<?> future = exe.submit(() -> "Hello"); // uses callable
+        //Runnable runnable = () -> System.out.println("Can be done like this way also");  // no return type for runnable
+        //Future<?> future = exe.submit(runnable);
+        Callable<String> callable = () -> "Has return type";
+        Future<?> future = exe.submit(callable);
         System.out.println(future.get()); // get() will block the execution until the task is complete.
+        if (future.isDone())
+            System.out.println("Task completed!");
         exe.shutdown();
     }
 }
