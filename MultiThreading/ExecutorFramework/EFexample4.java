d27d495731a6c298641c7ce16ed8e10f8ecfd669
Viv <121664534+vivisn00b@users.noreply.github.com>
2025-08-15 20:37:36 +0530

    Multi Threading (Executor Framework)

--- MultiThreading/ExecutorFramework/EFexample4.java
+++ MultiThreading/ExecutorFramework/EFexample4.java
@@ -9,14 +9,16 @@ public class EFexample4 {
         scheduler.schedule(() -> {
             System.out.println("Run after 2 secs by " + Thread.currentThread().getName());
         }, 2, TimeUnit.SECONDS);
-        // Task every 1 sec
+        // Task every 3 sec, delay is from start time (overlaps runs)
         scheduler.scheduleAtFixedRate(() -> {
             System.out.println("Repeating task on " + Thread.currentThread().getName());
-        }, 1, 1, TimeUnit.SECONDS);
+        }, 1, 3, TimeUnit.SECONDS);
+        // Task after 4 sec from end time of the previous task (no overlap)
+        scheduler.scheduleWithFixedDelay(() -> System.out.println("Task executed after 4 seconds by " + Thread.currentThread().getName()), 2, 4, TimeUnit.SECONDS);
         // Stop after 10 seconds
         scheduler.schedule(() -> {
             System.out.println("Shutting down scheduler");
-            scheduler.shutdown();
+            scheduler.shutdown();  // shutdown() must be triggered from inside another scheduled task because you decide dynamically when to stop the cycle.
         }, 10, TimeUnit.SECONDS);
     }
 }
