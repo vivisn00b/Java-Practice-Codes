164789b9fabda2e0267f4b02e93acfa3f7596933
Viv <121664534+vivisn00b@users.noreply.github.com>
2025-08-18 11:45:01 +0530

    Multi threading (CyclicBarrier)

---
+++ MultiThreading/CyclicBarrier/CBexample.java
@@ -0,0 +1,36 @@
+package MultiThreading.CyclicBarrier;
+
+import java.util.concurrent.*;
+
+public class CBexample {
+    public static void main(String[] args) {
+        int n = 3;
+        ExecutorService exec = Executors.newFixedThreadPool(n);
+        CyclicBarrier barrier = new CyclicBarrier(n);
+        exec.submit(new SubSystem(barrier));
+        exec.submit(new SubSystem(barrier));
+        exec.submit(new SubSystem(barrier));
+        System.out.println("Main thread...");
+//        barrier.getNumberWaiting();
+//        barrier.reset();
+//        barrier.getParties();
+        exec.shutdown();
+    }
+}
+
+class SubSystem implements Callable<Integer> {
+
+    private CyclicBarrier barrier;
+    public SubSystem(CyclicBarrier barrier) {
+        this.barrier = barrier;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println(Thread.currentThread().getName() + " service started");
+        Thread.sleep(1500);
+        System.out.println(Thread.currentThread().getName() + " is waiting...");
+        barrier.await();
+        return 0;
+    }
+}
