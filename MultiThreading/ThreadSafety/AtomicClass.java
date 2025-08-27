7036f4dad8cd4ec599db82411c7bada8ddba7b56
Viv <121664534+vivisn00b@users.noreply.github.com>
2025-08-27 15:47:54 +0530

    Multithreading (Atomic class)

--- MultiThreading/ThreadSafety/AtomicClass.java
+++ MultiThreading/ThreadSafety/AtomicClass.java
@@ -1,15 +1,25 @@
 package MultiThreading.ThreadSafety;
 
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class AtomicClass {
     private AtomicInteger counter = new AtomicInteger(0);
+    private AtomicBoolean flag = new AtomicBoolean(false);
 
-    public void increment() {
+    public void increment() throws InterruptedException {
         counter.incrementAndGet();
+        if (counter.get() == 2000) {
+            // thread-safe toggle
+            if (flag.compareAndSet(false, true)) {
+                Thread.sleep(1000);
+                System.out.println("Flag was false, now set to true");
+            }
+        }
     }
 
-    public int getCounter() {
+    public int getCounter() throws InterruptedException {
+        Thread.sleep(1500);
         return counter.get();
     }
 
@@ -17,12 +27,20 @@ public class AtomicClass {
         AtomicClass atomic = new AtomicClass();
         Thread t1 = new Thread(() -> {
             for (int i = 0; i < 1000; i++) {
-                atomic.increment();
+                try {
+                    atomic.increment();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
         Thread t2 = new Thread(() -> {
             for (int i = 0; i < 1000; i++) {
-                atomic.increment();
+                try {
+                    atomic.increment();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
         t1.start();
