50e5cee3ea8591848e4159dc9ae9759b3395cabe
vivisn00b <vivek.d1574@gmail.com>
2025-08-27 19:45:32 +0530

    Lambda

---
+++ LambdaExpressions/LambdaOperations.java
@@ -0,0 +1,18 @@
+package LambdaExpressions;
+
+@FunctionalInterface
+interface Functional {
+//    int operation(int a, int b);
+    void operation(int a, int b);
+}
+
+public class LambdaOperations {
+    public static void main(String[] args) {
+//        Functional add = (a, b) -> a+b;
+//        Functional multiply = (a, b) -> (a*b);
+//        System.out.println(add.operation(6, 3));
+//        System.out.println(multiply.operation(5, 5));
+        Functional add = (a, b) -> System.out.println("Sum: " + (a+b));
+        add.operation(5, 9);
+    }
+}
