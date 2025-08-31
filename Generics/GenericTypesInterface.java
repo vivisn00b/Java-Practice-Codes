499f554eab795c9c8bd9555d7765f8142a5fd15c
Viv <121664534+vivisn00b@users.noreply.github.com>
2025-08-31 15:57:21 +0530

    Generics

---
+++ Generics/GenericTypesInterface.java
@@ -0,0 +1,51 @@
+//Type Parameter Naming Conventions:
+//T: Type
+//E: Element (used in collections)
+//K: Key (used in maps)
+//V: Value (used in maps)
+//N: Number
+
+package Generics;
+
+interface Container<T> {
+    void set(T item);
+    T get();
+}
+
+// Implementing with a specific type
+class StringCointainer implements Container<String> {
+    private String item;
+
+    @Override
+    public void set(String item) {
+        this.item = item;
+    }
+
+    @Override
+    public String get() {
+        return item;
+    }
+}
+
+//Implementing a generic interface generically
+class GenericContainer<T> implements Container<T> {
+    private T item;
+
+    @Override
+    public void set(T item) {
+        this.item = item;
+    }
+
+    @Override
+    public T get() {
+        return item;
+    }
+}
+
+public class GenericTypesInterface {
+    public static void main(String[] args) {
+        StringCointainer sc = new StringCointainer();
+        sc.set("implemented with specific type");
+        System.out.println(sc.get());
+    }
+}
