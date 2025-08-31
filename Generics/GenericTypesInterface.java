0e105ef56c71605e90b9b384832550488fddcbd1
Viv <121664534+vivisn00b@users.noreply.github.com>
2025-08-31 19:36:15 +0530

    Generics

--- Generics/GenericTypesInterface.java
+++ Generics/GenericTypesInterface.java
@@ -12,8 +12,13 @@ interface Container<T> {
     T get();
 }
 
+interface SuperContainer<K, V> {
+    K getKey();
+    V getValue();
+}
+
 // Implementing with a specific type
-class StringCointainer implements Container<String> {
+class StringContainer implements Container<String> {
     private String item;
 
     @Override
@@ -42,14 +47,40 @@ class GenericContainer<T> implements Container<T> {
     }
 }
 
+//Generic Interfaces with Multiple Type Parameters
+class KeyValuePair<K, V> implements SuperContainer<K, V> {
+    private K key;
+    private V value;
+
+    public KeyValuePair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    @Override
+    public V getValue() {
+        return value;
+    }
+}
+
 public class GenericTypesInterface {
     public static void main(String[] args) {
-        StringCointainer sc = new StringCointainer();
+        Container<String> sc = new StringContainer();
+//        StringContainer sc = new StringContainer();
         sc.set("implemented with specific type");
         System.out.println(sc.get());
 
         GenericContainer<String> gc = new GenericContainer<>();
         gc.set("implemented with generics");
         System.out.println(gc.get());
+
+        KeyValuePair<String, Long> pair = new KeyValuePair<>("User ID", Thread.currentThread().getId());
+//        SuperContainer<String, Integer> pair = new KeyValuePair<>("Age", 30);
+        System.out.println(pair.getKey() + ": " + pair.getValue());
     }
 }
