eb3ec51f5ca1f45a17a3f6f34c06d72128265391
vivisn00b <vivek.d1574@gmail.com>
2025-09-06 12:50:49 +0530

    Generics

--- Generics/EnumGenerics.java
+++ Generics/EnumGenerics.java
@@ -22,11 +22,37 @@ enum Operation {
     }
 }
 
+// While we can’t define a generic type parameter at the enum declaration level because enums can’t be generic themselves
+// We can achieve a similar effect by using an enum with a generic class or by adding a generic interface.
+interface Calculator<T> {
+    T calculate(T a, T b);
+}
+
+enum ArithmeticOperation implements Calculator<Integer> {
+    ADD {
+        @Override
+        public Integer calculate(Integer a, Integer b) {
+            return a + b;
+        }
+    },
+    SUBTRACT {
+        @Override
+        public Integer calculate(Integer a, Integer b) {
+            return a - b;
+        }
+    };
+}
+
 public class EnumGenerics {
     public static void main(String[] args) {
         double result1 = Operation.ADD.apply(10, 20);
         double result2 = Operation.MULTIPLY.apply(5, 4);
-        System.out.println(result1);  // Output: 30.0
-        System.out.println(result2);  // Output: 20.0
+        System.out.println(result1);
+        System.out.println(result2);
+
+        int resultAdd = ArithmeticOperation.ADD.calculate(10, 5);
+        int resultSub = ArithmeticOperation.SUBTRACT.calculate(10, 5);
+        System.out.println(resultAdd);
+        System.out.println(resultSub);
     }
 }
