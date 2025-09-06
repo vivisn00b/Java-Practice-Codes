80ae6c48fc6cfcbc92150698e64c8c93b61112aa
vivisn00b <vivek.d1574@gmail.com>
2025-09-06 12:17:31 +0530

    Generics (method overloading)

--- Generics/GenericMethods.java
+++ Generics/GenericMethods.java
@@ -16,6 +16,14 @@ class Utils {
     public static <K, V> void printValue(K key, V value) {
         System.out.println("Key: " + key + " Value: " + value);
     }
+
+    public static  <T> void display(T element) {
+        System.out.println("Generic display: " + element);
+    }
+
+    public static void display(Integer element) {
+        System.out.println("Integer display: " + element);
+    }
 }
 
 public class GenericMethods {
@@ -29,5 +37,7 @@ public class GenericMethods {
         Utils.printArray(strArray);
         Utils.printValue(101, "Alice");
         Utils.printValue("Age", 25);
+        Utils.display(10000000);  // Calls Integer display
+        Utils.display("Generic method overloading");  // Calls generic display
     }
 }
