601a29f9e5aac4ff5d9d30d5d61c3a8c83a7c281
Viv <121664534+vivisn00b@users.noreply.github.com>
2025-09-21 19:00:04 +0530

    LinkedList

---
+++ Collections/LinkedList/LinkedListObjects.java
@@ -0,0 +1,32 @@
+package Collections.LinkedList;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+class Student {
+    String name;
+    int age;
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String toString() {
+        return name + " (" + age + ")";
+    }
+}
+
+public class LinkedListObjects {
+    public static void main(String[] args) {
+        LinkedList<Student> students = new LinkedList<>();
+        students.add(new Student("Alice", 20));
+        students.add(new Student("Bob", 22));
+        students.add(new Student("Charlie", 21));
+
+        students.forEach(System.out::println);
+
+        // Using iterator
+        Iterator<Student> it = students.iterator();
+        while (it.hasNext())
+            System.out.println(it.next());
+    }
+}
