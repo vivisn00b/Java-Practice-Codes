c21529d13e85191b28ee02b9260631b48728ef2e
vivisn00b <vivek.d1574@gmail.com>
2025-09-25 16:56:55 +0530

    Stack

---
+++ Collections/Stack/StackObjects.java
@@ -0,0 +1,22 @@
+package Collections.Stack;
+
+import java.util.Stack;
+
+class Book {
+    String title;
+    Book(String title) { this.title = title; }
+    public String toString() { return title; }
+}
+
+public class StackObjects {
+    public static void main(String[] args) {
+        Stack<Book> books = new Stack<>();
+        books.push(new Book("Java"));
+        books.push(new Book("Python"));
+        books.push(new Book("C++"));
+
+        while (!books.empty()) {
+            System.out.println("Reading: " + books.pop());
+        }
+    }
+}
