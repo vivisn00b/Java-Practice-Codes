55960b390ea9411ddf0df0820f7628549007b672
Viv <121664534+vivisn00b@users.noreply.github.com>
2025-10-01 17:11:34 +0530

    HashMap

---
+++ Collections/MapInterface/HashMap/HashMapFunctions.java
@@ -0,0 +1,37 @@
+package Collections.MapInterface.HashMap;
+
+import java.util.HashMap;
+
+public class HashMapFunctions {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("A", 10);              // put
+        map.put("B", 20);
+        map.put("C", 30);
+
+        HashMap<String, Integer> copyMap = new HashMap<>();
+        copyMap.put("D", 40);
+        copyMap.putAll(map);           // putAll
+
+        System.out.println("get('A'): " + map.get("A"));               // get
+        System.out.println("containsKey('B'): " + map.containsKey("B"));      // containsKey
+        System.out.println("containsValue(40): " + copyMap.containsValue(40));  // containsValue
+        System.out.println("size(): " + map.size());                   // size
+        System.out.println("isEmpty(): " + map.isEmpty());             // isEmpty
+        System.out.println("keySet(): " + map.keySet());               // keySet
+        System.out.println("values(): " + map.values());               // values
+        System.out.println("entrySet(): " + map.entrySet());           // entrySet
+
+        map.replace("C", 300);                    // replace
+        map.remove("B");                          // remove
+        copyMap.clear();                          // clear
+        System.out.println("clone(): " + map.clone());  // clone
+
+        map.compute("A", (k, v) -> v + 5);            // compute
+        // System.out.println("After compute fnc: " + map);
+        map.computeIfAbsent("D", k -> 100);           // computeIfAbsent
+        map.computeIfPresent("A", (k, v) -> v * 2);   // computeIfPresent
+        map.merge("A", 10, (oldV, newV) -> oldV + newV); // merge
+        System.out.println("map after manipulations: " + map);
+    }
+}
