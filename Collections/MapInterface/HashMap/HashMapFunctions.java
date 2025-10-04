d9b323662674b981fd8844175aaaf83e87482df7
Viv <121664534+vivisn00b@users.noreply.github.com>
2025-10-04 23:34:05 +0530

    HashMap

--- Collections/MapInterface/HashMap/HashMapFunctions.java
+++ Collections/MapInterface/HashMap/HashMapFunctions.java
@@ -33,5 +33,15 @@ public class HashMapFunctions {
         map.computeIfPresent("A", (k, v) -> v * 2);   // computeIfPresent
         map.merge("A", 10, (oldV, newV) -> oldV + newV); // merge
         System.out.println("map after manipulations: " + map);
+
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("Shubham", 91);
+        hashMap.put("Bob", 80);
+        hashMap.put("Akshit", 78);
+        // Retrieves the value associated with a given key if it exists in the map. If the key does not exist, it returns a default value provided by the user.
+        Integer res = hashMap.getOrDefault("Vivek", 0);
+        System.out.println("Is Vivek present? " + res);  // 0
+        hashMap.put("Shubham", 92);
+        System.out.println(hashMap);
     }
 }
